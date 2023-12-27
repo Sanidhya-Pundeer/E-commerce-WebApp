7d51971d194df378ac06d1531722955d9232782a
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2023-12-27 20:12:15 +0530

    Update SqlCon.java

--- src/main/java/org/ncu/E_commerce/Dao/SqlCon.java
+++ src/main/java/org/ncu/E_commerce/Dao/SqlCon.java
@@ -9,8 +9,8 @@ public class SqlCon {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			String URL = "jdbc:mysql://localhost/mvc";
-			String user = "Riya";
-			String pass = "771078";
+			String user = "root";
+			String pass = "scott";
 			Connection con = DriverManager.getConnection(URL, user, pass);
 			return con;
 		} catch (Exception e) {
